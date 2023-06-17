123126ca33f4ce02a075eb2ec7c9b3a4d97e8c43
AJ2790 <jatin.aggarwal2790@live.com>
2023-06-17 19:21:48 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,9 +1,8 @@
 module "network" {
   source  = "app.terraform.io/jatinaggarwal2790-training/network/azurerm"
-  version = "5.3.0"
+  version = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.6.0.0/16"]
   subnet_prefixes     = ["10.6.1.0/24", "10.6.2.0/24"]
   subnet_names        = ["subnet1", "subnet2"]
-  use_for_each = true
 }
